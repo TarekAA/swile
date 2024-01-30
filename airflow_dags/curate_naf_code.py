6ead2102a34266b6609a3ebea8b4a5f0dc63007e
tarek <tarek.alashkar@gmail.com>
2024-01-30 02:26:33 +0100

    fix: Replace ORM operation with direct database connector ones
    Due to a conflict in SQLAlchemy version the default pandas method to write and read dataframes using ORM is broken and thus had to revert back to a method that doesn't use SQLAlchemy.
    The conflict is caused by flask-appBuilder (airflow dependancy) requiring a version of SQLAlchemy incompatible with pandas and psycopg2.

--- airflow_dags/curate_naf_code.py
+++ airflow_dags/curate_naf_code.py
@@ -16,6 +16,14 @@ def get_postgres_engine():
     return engine
 
 
+def get_postgres_db_cursor():
+    pg_config = dict(**PG_CONFIG)
+    pg_config['dbname'] = DB_NAME
+    conn = psycopg2.connect(**pg_config)
+    cur = conn.cursor()
+    return conn, cur
+
+
 @dataclass
 class NAFAPIRequest:
     token: str
@@ -36,7 +44,10 @@ class NAFAPIRequest:
         if response.status_code == 200:
             # Parse the JSON response
             data = response.json()
-            return data
+            naf_code = data.get('etablissement').get('uniteLegale').get('activitePrincipaleUniteLegale')
+            # naf_code = (data.get('etablissement').get('periodesEtablissement')[0]
+            #             .get('activitePrincipaleEtablissement'))
+            return naf_code
         else:
             print(f'Failed to retrieve data for siret={siret} ; reason: {response.status_code}')
             return None
@@ -46,6 +57,7 @@ class NAFAPIRequest:
 @dataclass
 class NAFAPIRequester(NAFAPIRequest):
     siret_list: list[str] = field(default=None)
+    schema: str = field(default='general')
     table_name: str = field(default='siret_naf_staging')
     naf_code_df: pd.DataFrame = field(init=False)
 
@@ -58,19 +70,49 @@ class NAFAPIRequester(NAFAPIRequest):
         self.naf_code_df = naf_code_df
 
     def write_to_db(self):
-        engine = get_postgres_engine()
-        self.naf_code_df.to_sql(self.table_name, engine, if_exists='append', index=False)
+        conn, cur = get_postgres_db_cursor()
+        columns = ', '.join(self.naf_code_df.columns)
+        sql_template = f"INSERT INTO {self.schema}.{self.table_name} ({columns}) VALUES "
+
+        values_list = []
+        for index, row in self.naf_code_df.iterrows():
+            items_ = [f"'{item}'" if isinstance(item, str) else str(item) for item in row]
+            values = ', '.join(items_)
+            values_list.append(f"({values})")
+        sql_statement = sql_template + ', '.join(values_list) + ";"
+
+        cur.execute(sql_statement)
+        conn.commit()
+        cur.close()
+        conn.close()
 
 
 def get_siret_to_be_mapped() -> list[str]:
-    engine = get_postgres_engine()
-    df = pd.read_sql('SELECT DISTINCT siret FROM siret_to_be_mapped', engine)
-    siret_list = df['siret'].tolist()
+    conn, cur = get_postgres_db_cursor()
+    cur.execute('SELECT DISTINCT siret FROM general.siret_to_be_mapped')
+    res = cur.fetchall()
+    siret_list = [val[0] for val in res]
     return siret_list
 
 
 def generate_siret_to_be_mapped_table():
+    # we run siret_naf here to make sure the table get created when it's run for the first time
+    # we will recall back at the end
     cli_args = ["run", "--select", "siret_naf", "siret_to_be_mapped",
+                "--project-dir", "swile/dbt/swile",
+                "--profiles-dir", "swile/"]
+    run_dbt_project(cli_args)
+
+
+def generate_siret_naf_table():
+    cli_args = ["run", "--select", "siret_naf",
+                "--project-dir", "swile/dbt/swile",
+                "--profiles-dir", "swile/"]
+    run_dbt_project(cli_args)
+
+
+def generate_spent_table():
+    cli_args = ["run", "--select", "spent",
                 "--project-dir", "swile/dbt/swile",
                 "--profiles-dir", "swile/"]
     run_dbt_project(cli_args)
@@ -78,12 +120,14 @@ def generate_siret_to_be_mapped_table():
 
 def write_naf_codes():
     siret_list = get_siret_to_be_mapped()
-    naf_requester = NAFAPIRequester(token=INSEE_API_TOKEN, siret_list=siret_list,
+    naf_requester = NAFAPIRequester(token=INSEE_API_TOKEN,
+                                    siret_list=siret_list,
+                                    schema='general',
                                     table_name='siret_naf_staging')
+    naf_requester.get_all_naf_codes()
     naf_requester.write_to_db()
 
 
 if __name__ == "__main__":
-    # NAFAPIRequest('ee03bc97-73cf-3756-8c55-e6c2bedcebd9').get_naf_code('41622001001280')
     generate_siret_to_be_mapped_table()
     write_naf_codes()
